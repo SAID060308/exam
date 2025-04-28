b2853c017409bb881487a54546b20c89f6160af4
Said <said060308@gmail.com>
2025-04-28 15:19:34 +0500

    1 test

---
+++ src/test/java/com/example/examteam/ExamTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
